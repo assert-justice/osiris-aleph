0f11d39aa3cc2438272417458ddba9fcb2578232
Riley <waskerdu@gmail.com>
2025-12-07 18:18:30 -0800

    remove non fuctioning code from test utils

--- scripts/tests/TestUtils.cs
+++ scripts/tests/TestUtils.cs
@@ -8,15 +8,4 @@ public static class TestUtils
         string filepath = $"../../../../../scripts/tests/example_files/{filename}";
         return File.ReadAllText(filepath);
     }
-    public static bool FileExists(string filename)
-    {
-        string filepath = $"../../../../../scripts/tests/example_files/{filename}";
-        return File.Exists(filepath);
-    }
-    public static void WriteSnapshot(string filename, string contents, bool overwrite = false)
-    {
-        string filepath = $"../../../../../scripts/tests/example_files/{filename}";
-        if(!overwrite && File.Exists(filepath)) return;
-        File.WriteAllText(filepath, contents);
-    }
 }
