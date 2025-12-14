7cbaa9b59120ff45b0c74805a11a943f840a1294
Riley <waskerdu@gmail.com>
2025-12-14 15:24:16 -0800

    add simple mock filesystem for testing

--- scripts/utlis/OsirisFileAccess.cs
+++ scripts/utlis/OsirisFileAccess.cs
@@ -1,5 +1,5 @@
-
 using System;
+using System.Collections.Generic;
 using Godot;
 
 namespace Osiris
@@ -8,18 +8,39 @@ namespace Osiris
     {
         static Func<string, string> Reader = filepath =>
         {
-            using var file = FileAccess.Open("res://" + filepath, FileAccess.ModeFlags.Read);
+            using var file = FileAccess.Open(filepath, FileAccess.ModeFlags.Read);
             return file.GetAsText();
         };
-        static Action<string, string> Writer;
+        static Action<string, string> Writer = (filepath, contents) =>
+        {
+            using var file = FileAccess.Open(filepath, FileAccess.ModeFlags.Read);
+            file.StoreString(contents);
+        };
         static Func<string, bool> Checker = FileAccess.FileExists;
+        static readonly Dictionary<string, string> MockFilesystem = [];
         static bool InTestMode = false;
         public static void EnterTestMode()
         {
             if(InTestMode) return;
-            Reader = TestUtils.ReadFile;
-            Checker = TestUtils.FileExists;
+            // Reader = TestUtils.ReadFile;
+            // Checker = TestUtils.FileExists;
             InTestMode = true;
+            Reader = filepath =>
+            {
+                if(TestUtils.FileExists(filepath)) return TestUtils.ReadFile(filepath);
+                if(MockFilesystem.TryGetValue(filepath, out string contents)) return contents;
+                return "";
+            };
+            Checker = filepath =>
+            {
+                if(TestUtils.FileExists(filepath)) return true;
+                if(MockFilesystem.ContainsKey(filepath)) return true;
+                return false;
+            };
+            Writer = (filepath, contents) =>
+            {
+                MockFilesystem.Add(filepath, contents);
+            };
         }
         public static string ReadFile(string filename)
         {
