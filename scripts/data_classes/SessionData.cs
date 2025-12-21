b10c44ca4c71e83cbce07ef26e0d470bd50a3ff8
Riley <waskerdu@gmail.com>
2025-12-20 22:56:35 -0800

    start on sessions

---
+++ scripts/data_classes/SessionData.cs
@@ -0,0 +1,16 @@
+using Prion.Node;
+
+namespace Osiris.DataClass;
+
+public class SessionData : IDataClass<SessionData>
+{
+    public static bool TryFromNode(PrionNode node, out SessionData data)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public PrionNode ToNode()
+    {
+        throw new System.NotImplementedException();
+    }
+}
