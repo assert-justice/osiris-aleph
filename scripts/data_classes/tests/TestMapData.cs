ee4ce066da2b3e3b73ba1bee77a7a400d891152a
Riley <waskerdu@gmail.com>
2025-12-20 17:32:45 -0800

    start to mock map

--- scripts/data_classes/tests/TestMapData.cs
+++ scripts/data_classes/tests/TestMapData.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Osiris.DataClass.Tests;
@@ -6,6 +7,31 @@ namespace Osiris.DataClass.Tests;
 public class TestMapData : TestDataClass<MapData>
 {
     public TestMapData() : base("map"){}
+    public override MapData Mock()
+    {
+        int numBlockers = MockData.Rng.Next(100, 200);
+        int numTileGroups = MockData.Rng.Next(50);
+        int numLayers = MockData.Rng.Next(3, 16);
+        // Thanks, I hate it
+        // TestBlockerData testBlocker = new();
+        // TestTileGroupData testBlocker = new();
+        // TestBlockerData testBlocker = new();
+        MapData data = new(Guid.NewGuid())
+        {
+            DisplayName = MockData.GetRandomIdent(),
+            Size = MockData.GetRandomVector2I(0, 100, 0, 100),
+            CellWidth = MockData.GetRandomFloat(32, 256),
+            UsersPresent = MockData.GetRandomSet(Guid.NewGuid, MockData.Rng.Next(16)),
+            LightingEnabled = MockData.GetRandomBool(),
+            BackgroundColor = MockData.GetRandomColor(),
+            BorderColor = MockData.GetRandomColor(),
+            GridVisible = MockData.GetRandomBool(),
+            GridColor = MockData.GetRandomColor(),
+            GridLineWidth = MockData.GetRandomFloat(1, 16),
+            Blockers = MockData.GetRandomList(new TestBlockerData().Mock, numBlockers),
+        };
+        return data;
+    }
 
     [TestMethod]
     public void LoadAndValidateActor()
