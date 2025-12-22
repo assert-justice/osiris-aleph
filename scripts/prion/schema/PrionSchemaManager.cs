853fb1b20a04f45229b0a4c35e84cf33e892a878
Riley <waskerdu@gmail.com>
2025-12-21 16:57:48 -0800

    minor additions/changes to schema parser

--- scripts/prion/schema/PrionSchemaManager.cs
+++ scripts/prion/schema/PrionSchemaManager.cs
@@ -8,17 +8,29 @@ public static class PrionSchemaManager
 {
     static readonly Dictionary<Type, PrionSchema> SchemasByType = [];
     public static readonly Dictionary<string, PrionSchema> SchemasByName = [];
-    public static void RegisterSchema(Type type, PrionSchema schema)
+    public static void RegisterSchema(PrionSchema schema, Type type)
     {
-        // PrionSchema.SetManager(this);
         SchemasByName[schema.Name] = schema;
         SchemasByType[type] = schema;
+    }
+    public static void RegisterSchema(PrionSchema schema)
+    {
+        SchemasByName[schema.Name] = schema;
     }
     public static bool Validate(Type type, PrionNode node, out string error)
     {
         if(!SchemasByType.TryGetValue(type, out PrionSchema schema))
         {
-            error = $"Could not find schema for {type}.";
+            error = $"Could not find schema for type '{type}'.";
+            return false;
+        }
+        return schema.TryValidate(node, out error);
+    }
+    public static bool Validate(string name, PrionNode node, out string error)
+    {
+        if(!SchemasByName.TryGetValue(name, out PrionSchema schema))
+        {
+            error = $"Could not find schema for name '{name}'.";
             return false;
         }
         return schema.TryValidate(node, out error);
--- scripts/utlis/OsirisSystem.cs
+++ scripts/utlis/OsirisSystem.cs
@@ -66,7 +66,7 @@ namespace Osiris
                 ReportError(error);
                 return;
             }
-            PrionSchemaManager.RegisterSchema(type, prionSchema);
+            PrionSchemaManager.RegisterSchema(prionSchema, type);
         }
         public static void EnterTestMode()
         {
